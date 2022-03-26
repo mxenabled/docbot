571da389c45967c5fd1eb2ee25a5bd788df81672
Garrett Thornburg <film42@gmail.com>
2022-03-26 09:16:22 -0600

    Exclude zero values from the CRD generator to make argo happy

--- docbot-controller/build.rs
+++ docbot-controller/build.rs
@@ -9,6 +9,16 @@ fn main() {
     // NOTE: The namespace "default" is expected for a namespaced CRD?
     crd.metadata.namespace = Some("default".into());
 
+    // These fields show up as Some([]) in rust, but go will exclude because
+    // they are zero values. If you see Argo continue to cause issues sync'ing,
+    // check a compact diff and see if any new fields show up.
+    crd.spec.names.categories = None;
+    crd.spec.names.short_names = None;
+    crd.spec
+        .versions
+        .iter_mut()
+        .for_each(|version| version.additional_printer_columns = None);
+
     // Write to file.
     let schema = serde_yaml::to_string(&crd).unwrap();
     let crate_dir = std::env::var_os("CARGO_MANIFEST_DIR").unwrap();
